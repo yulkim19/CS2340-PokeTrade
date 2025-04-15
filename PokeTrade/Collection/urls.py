7ab10789ee11272dbfacae5b5abc58616cfedfa3
Avril <femiprattavril@gmail.com>
2025-04-15 13:55:46 -0400

    fixed page linking issue and navbar styling

--- PokeTrade/Collection/urls.py
+++ PokeTrade/Collection/urls.py
@@ -5,6 +5,5 @@ from .views import generate_pokemon_view
 
 urlpatterns = [
     path("", views.index, name="Collection.index"),
-    path("about", views.about, name="Collection.about"),
     path('generate-pokemon/', generate_pokemon_view, name='generate_pokemon'),
 ]
--- PokeTrade/home/urls.py
+++ PokeTrade/home/urls.py
@@ -1,10 +1,6 @@
-# from django.conf.urls.static import static
-# from django.conf import settings
+from django.urls import path
+from . import views
 
-# from django.contrib import admin
-# from django.urls import include, path
-# from . import views
-
-# urlpatterns = [
-#     path('', views.home_page, name='home'),
-# ]
+urlpatterns = [
+    path('', views.home_page, name='home'),
+ ]
