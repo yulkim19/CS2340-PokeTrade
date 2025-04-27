26a6a70b7608c5b3fff2c4985a7995146b1aa89e
Avril <femiprattavril@gmail.com>
2025-04-27 04:03:53 -0400

    chat split view functionality

--- PokeTrade/chats/views.py
+++ PokeTrade/chats/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Message
 
@@ -15,6 +15,17 @@ def chat_dashboard(request, username=None):
             sender__username__in=[request.user.username, username],
             receiver__username__in=[request.user.username, username]
         ).order_by('timestamp')
+
+        if request.method == "POST":
+            message_text = request.POST.get('message')
+            if message_text:
+                receiver = User.objects.get(username=username)
+                new_message = Message.objects.create(
+                    sender = request.user,
+                    receiver = receiver,
+                    message = message_text
+                )
+                return redirect('chats.chat_dashboard', username=username)
 
         context = {
             'room_name': room_name,
